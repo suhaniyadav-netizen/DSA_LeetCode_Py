d3eb742a8ec6a33a0c6c96948d2cf1988e3c11ea
suhaniyadav-netizen <suhani.yadavmicro@gmail.com>
2026-02-09 21:46:39 +0530

    New Solution added

--- questions/242.Valid Anagram.py
+++ questions/242.Valid Anagram.py
@@ -18,3 +18,23 @@ class Solution(object):
             if count[char] < 0:
                 return False
         return True
+    
+# time complexity is O(n) and space complexity is O(n)
+# 
+# 
+# 
+# another solution ( accepted by leetcode in less ms speed ) 
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        for i in set(s):
+            if s.count(i) != t.count(i):
+                return False
+        return True
+           
