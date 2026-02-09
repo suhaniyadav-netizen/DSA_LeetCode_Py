e4b23030b691b65251baa6f4a0b93ad6d49c78ef
suhaniyadav-netizen <suhani.yadavmicro@gmail.com>
2026-02-09 11:52:54 +0530

    Problem Number 268 : MISSING NUMBER

--- practise.py
+++ practise.py
@@ -298,3 +298,17 @@ class Solution(object):
             if count[char] < 0:
                 return False
         return True
+
+
+# LEETCODE PROBLEM NUMBER 268 : MISSING NUMBER
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        total = n * (n + 1) // 2
+        asum = sum(nums)
+        ans = total - asum
+        return ans
