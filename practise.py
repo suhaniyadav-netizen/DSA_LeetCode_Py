5deb0ec667b6405ed186e840ea0ee3bb08dac2e4
suhaniyadav-netizen <suhani.yadavmicro@gmail.com>
2026-02-09 19:31:10 +0530

    LEETCODE PROBLEM NO 217 : CONTAINS DUPLICATE

--- practise.py
+++ practise.py
@@ -208,18 +208,7 @@ class Solution(object):
 
 
 # 
-# # LEETCODE PROBLEM NUMBER 217 : CONTAINS DUPLICATE
-# 
-# class Solution(object):
-    def containsDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """  
-        if len(nums) != len(set(nums)):
-            return True
-        return False
-# time complexity is O(n) and space complexity is O(n)
+#
 
 
 
