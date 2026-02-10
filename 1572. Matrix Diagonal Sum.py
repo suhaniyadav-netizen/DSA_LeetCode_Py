7d3080ef0a3ccb3190604266a7239110d346d1a6
suhaniyadav-netizen <suhani.yadavmicro@gmail.com>
2026-02-10 22:39:29 +0530

    LEETCODE PROBLEM NO 1572 : MATRIX DIAGONAL SUM

---
+++ 1572. Matrix Diagonal Sum.py
@@ -0,0 +1,19 @@
+# LEETCODE PROBLEM NO 1572 : MATRIX DIAGONAL SUM
+
+# Given a square matrix mat, return the sum of the matrix diagonals.
+# Only include the sum of all the elements on the primary diagonal and all the elements on 
+# the secondary diagonal that are not part of the primary diagonal.
+
+class Solution(object):
+    def diagonalSum(self, mat):
+        """
+        :type mat: List[List[int]]
+        :rtype: int
+        """
+        sum = 0
+        for i in range(len(mat)):
+            sum += mat[i][i]
+            if i != len(mat) - 1 - i:
+                sum += mat[i][len(mat) - 1 - i]
+        return sum
+    
