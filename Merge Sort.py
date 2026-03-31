2f2ca8d96fb95f74b814bdd5f46e2173b2e52601
suhaniyadav-netizen <suhani.yadavmicro@gmail.com>
2026-03-31 15:58:29 +0530

    feat: Merge Sort

---
+++ Merge Sort.py
@@ -0,0 +1,64 @@
+# Merge sort is a sorting algorithm that:
+# divides the array into smaller parts
+# sorts those smaller parts
+# merges them back in sorted order
+
+# It follows divide and conquer.
+
+# Example : [8, 3, 5, 4, 7, 6, 1, 2]
+
+# Step 1: Divide
+# [8, 3, 5, 4] and [7, 6, 1, 2]
+
+# Step 2: Divide again
+# [8, 3] [5, 4] [7, 6] [1, 2]
+
+# Step 3: Divide again
+# [8] [3] [5] [4] [7] [6] [1] [2]
+
+# Now all are single elements, so all are sorted.
+# Step 4: Merge
+# [8] + [3] -> [3, 8]
+# [5] + [4] -> [4, 5]
+# [7] + [6] -> [6, 7]
+# [1] + [2] -> [1, 2]
+
+# Step 5: Merge bigger parts
+# [3, 8] + [4, 5] -> [3, 4, 5, 8]
+# [6, 7] + [1, 2] -> [1, 2, 6, 7]
+
+# Step 6: Final merge
+# [3, 4, 5, 8] + [1, 2, 6, 7] -> [1, 2, 3, 4, 5, 6, 7, 8]
+
+           
+def merge_sort(arr):                            # Sort Code Only
+    if len(arr) <= 1:                           # Base case: if array has 0/1 element, it's sorted
+        return arr
+
+    mid = len(arr) // 2                         # find middle index
+    left = merge_sort(arr[:mid])                # Recursively sort left half
+    right = merge_sort(arr[mid:])               # Recursively sort right half
+
+    return merge(left, right)
+
+
+def merge(left, right):                         # Merge Code Only 
+    result = []
+    i = 0
+    j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+
+    return result
+
+# Time Complexity : O(n log n) in all cases
+# Space Complexity : O(n) 
