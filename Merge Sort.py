659c778bd18520baca3d7274935fa5718882f38e
suhaniyadav-netizen <suhani.yadavmicro@gmail.com>
2026-03-31 16:04:54 +0530

    updated merge sort

--- Merge Sort.py
+++ Merge Sort.py
@@ -50,3 +50,41 @@ def merge(left, right):                         # Merge Code Only
 
 # Time Complexity : O(n log n) in all cases
 # Space Complexity : O(n) 
+
+
+
+# LEETCODE Format CODE
+
+class Solution(object):
+    def sortArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        def merge_sort(arr):
+            if len(arr) <= 1:
+                return arr
+
+            mid = len(arr) // 2
+            left = merge_sort(arr[:mid])
+            right = merge_sort(arr[mid:])
+
+            return merge(left, right)
+
+        def merge(left, right):
+            result = []
+            i = j = 0
+
+            while i < len(left) and j < len(right):
+                if left[i] <= right[j]:
+                    result.append(left[i])
+                    i += 1
+                else:
+                    result.append(right[j])
+                    j += 1
+
+            result.extend(left[i:])
+            result.extend(right[j:])
+            return result
+
+        return merge_sort(nums)
