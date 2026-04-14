17cc0f90774d7de4a025892ff7f90beb6c4f2c60
suhaniyadav-netizen <suhani.yadavmicro@gmail.com>
2026-04-14 22:41:26 +0530

    feat:Leetcode problem 100 : Same Tree - Solution

--- questions/35. Search Insert Position.py
+++ questions/35. Search Insert Position.py
@@ -11,3 +11,24 @@
 # Example 2:
 # Input: p = [1,2], q = [1,null,2]
 # Output: false
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: Optional[TreeNode]
+        :type q: Optional[TreeNode]
+        :rtype: bool
+        """
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
