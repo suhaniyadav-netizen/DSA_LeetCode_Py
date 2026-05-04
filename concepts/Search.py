dcd1a9cf7b6df3ad85760d7a44124a09633de54c
suhaniyadav-netizen <suhani.yadavmicro@gmail.com>
2026-05-04 21:42:21 +0530

    feat: Binary Search

--- concepts/Search.py
+++ concepts/Search.py
@@ -1,4 +1,6 @@
+
 # LINEAR SEARCH
+
 def linear_search(arr, target):
     for i in range(len(arr)):
         if arr[i] == target:
@@ -7,3 +9,18 @@ def linear_search(arr, target):
 
 #time complexity: O(n)
 #space complexity: O(1)
+
+
+# BINARY SEARCH
+
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = left + (right - left) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1      
+    return -1
